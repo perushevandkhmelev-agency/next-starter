df12416203f9ca498ce8cff8db6b46518eaa7e22
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2020-08-04 22:38:29 +0300

    Renamed style.js to styles.js

--- utils/globalStyles.js
+++ utils/globalStyles.js
@@ -1,4 +1,4 @@
-import { colors } from 'utils/style'
+import { colors } from 'utils/styles'
 
 export default function GlobalStyles() {
   return (
--- utils/meta.js
+++ utils/meta.js
@@ -1,5 +1,5 @@
 import Helmet from 'react-helmet'
-import { colors } from 'utils/style'
+import { colors } from 'utils/styles'
 import config from 'config'
 
 const dev = process.env.NODE_ENV !== 'production'
