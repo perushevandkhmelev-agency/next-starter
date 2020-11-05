71822409d9aead74ab7b8a7edf24deaa06054ba5
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2020-11-05 10:51:11 +0300

    Removed dotenv

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-require('dotenv').config()
-
 const withPlugins = require('next-compose-plugins')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
 
 module.exports = withPlugins([withImages, withFonts], {
-  env: {
-    API_URL: process.env.API_URL,
-    AUTHORIZATION: process.env.AUTHORIZATION
-  },
   webpack: (config) => {
     const originalEntry = config.entry
     config.entry = async () => {
