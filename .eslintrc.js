86986595b57ae90cf0fdfcc20d98199f26ce9552
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2020-11-07 17:15:41 +0300

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,13 @@ module.exports = {
       jsx: true
     }
   },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
   env: {
     browser: true,
     amd: true,
     node: true
   },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
@@ -26,6 +22,32 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+    'global-require': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'import/no-absolute-path': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-no-bind': [
+      'error',
+      {
+        ignoreDOMComponents: false,
+        ignoreRefs: true,
+        allowArrowFunctions: false,
+        allowFunctions: false,
+        allowBind: false
+      }
+    ],
     'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      'babel-module': {
+        'babel-plugin-module-resolver': { root: ['./'] }
+      }
+    }
   }
 }
