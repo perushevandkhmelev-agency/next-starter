9599b60ef305ee46de9a8f22d9c47532cab077ad
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2021-04-05 09:32:23 +0300

    Add eslint-webpack-plugin, stylelint-webpack-plugin & fork-ts-checker-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const withPlugins = require('next-compose-plugins')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
 
+const ESLintPlugin = require('eslint-webpack-plugin')
+const StylelintPlugin = require('stylelint-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = withPlugins([withImages, withFonts], {
-  webpack: (config) => {
+  webpack: (config, { dev, isServer }) => {
     const originalEntry = config.entry
     config.entry = async () => {
       const entries = await originalEntry()
@@ -13,6 +17,16 @@ module.exports = withPlugins([withImages, withFonts], {
       }
 
       return entries
+    }
+
+    if (dev && isServer) {
+      config.plugins.push(
+        ...[
+          new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
+          new StylelintPlugin({ files: '**/*.{js,jsx,ts,tsx}' }),
+          new ForkTsCheckerWebpackPlugin()
+        ]
+      )
     }
 
     config.node = config.node || {}
