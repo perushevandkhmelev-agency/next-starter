9e33b399aca99640b61f5a111ad2c1ac21708c06
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2021-05-31 19:08:50 +0300

    Remove custom polyfills

--- next.config.js
+++ next.config.js
@@ -2,22 +2,4 @@ const withPlugins = require('next-compose-plugins')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
 
-module.exports = withPlugins([withImages, withFonts], {
-  webpack: (config) => {
-    const originalEntry = config.entry
-    config.entry = async () => {
-      const entries = await originalEntry()
-
-      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
-        entries['main.js'].unshift('./polyfills.js')
-      }
-
-      return entries
-    }
-
-    config.node = config.node || {}
-    config.node.fs = 'empty'
-
-    return config
-  }
-})
+module.exports = withPlugins([withImages, withFonts])
--- polyfills.js
+++
@@ -1,3 +0,0 @@
-import 'core-js/features/array/find'
-import 'core-js/features/array/filter'
-import 'core-js/features/string/repeat'
