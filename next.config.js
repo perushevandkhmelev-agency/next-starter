061e938093bd59fa70bf63390dd26e05ac6b30aa
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2021-05-31 17:09:11 +0300

    Remove lint webpack plugins

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@ const withPlugins = require('next-compose-plugins')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
 
-const ESLintPlugin = require('eslint-webpack-plugin')
-const StylelintPlugin = require('stylelint-webpack-plugin')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-
 module.exports = withPlugins([withImages, withFonts], {
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config) => {
     const originalEntry = config.entry
     config.entry = async () => {
       const entries = await originalEntry()
@@ -17,16 +13,6 @@ module.exports = withPlugins([withImages, withFonts], {
       }
 
       return entries
-    }
-
-    if (dev && isServer) {
-      config.plugins.push(
-        ...[
-          new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
-          new StylelintPlugin({ files: '**/*.{js,jsx,ts,tsx}' }),
-          new ForkTsCheckerWebpackPlugin()
-        ]
-      )
     }
 
     config.node = config.node || {}
