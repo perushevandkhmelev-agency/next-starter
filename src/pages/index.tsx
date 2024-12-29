b82f2c2647be26fbe5178c6e008930d1b80edfb0
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2024-12-29 09:36:41 +0000

    Update packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,18 @@ import Icon from '#/components/Icon'
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">
-        <Icon name="heart" />
-        Hello world!
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <ol className="list-inside list-decimal text-center text-sm sm:text-left">
+        <li className="mb-2">
+          Get started by editing{' '}
+          <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
+            src/pages/index.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <Icon name="heart" className="mt-6 block text-red-500" />
     </main>
   )
 }
