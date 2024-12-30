c46ab7e3352b754a2d00147482a1018916ba65e2
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2024-12-30 18:58:43 +0000

    Add hoverOnlyWhenSupported

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
